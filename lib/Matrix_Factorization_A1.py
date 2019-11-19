19c055edf8ff618649ab6e11ad9d437dddbcd0a5
jevousvois <47004410+jevousvois@users.noreply.github.com>
2019-11-18 23:57:54 -0500

    MF_A1 update
    upload gradesc_dynamic in MF_A1

--- lib/Matrix_Factorization_A1.py
+++ lib/Matrix_Factorization_A1.py
@@ -105,8 +105,118 @@ class Matrix_Factorization:
 
             print(m, 'user_latent', user_latent)
 
-    def gradesc_dynamic(self,f=10,lam=0.3, batch = 50, lrate=0.01, maxiter=10, stopping_deriv=0.01):
-        return
+    def gradesc_dynamic(self,f=10,lam=0.3, batch=50, lrate=0.01, maxiter=10, stopping_deriv=0.01, bin_num=30, gamma=0.03, power=0.25):
+        # random assign value to matrix p and q
+        user_latent = np.random.randn(f,self._u)*0.01
+        user_latent = pd.DataFrame(user_latent)
+        user_latent.columns = self._data['userId'].unique().tolist()
+
+        item_latent = np.random.randn(f,self._i)*0.01
+        item_latent = pd.DataFrame(item_latent)
+        item_latent.columns = self._data['movieId'].unique().tolist()
+
+        ########## bias of item with respect to time #########
+
+        # distribute to bins
+        bin_width = (self._data['timestamp'].max()-self._data['timestamp'].min())//bin_num
+        bin_loc = (self._data['timestamp']-self._data['timestamp'].min())//bin_width
+        # adding info to origin dataset
+        self._data['bin_loc'] = bin_loc
+        item_bin_bias = self._data.groupby(['movieId','bin_loc']).mean()['rating']
+        # bias: b_i
+        total_mean = np.mean(self._data['rating'])
+        item_mean = self._data.groupby('movieId').mean()['rating']
+        item_bias = pd.DataFrame(item_mean - total_mean)
+        self._item_bias = pd.DataFrame(item_mean - total_mean)
+        # item dynamic bias: b_i_Bin(t)+b_i
+        for item_id in item_bias.index:
+          item_bin_bias[item_bin_bias.index.get_level_values(0)==item_id] += self._item_bias['rating'][item_id]
+        self._item_bin_bias = item_bin_bias
+
+        ########### bias of user with respect to time ###########
+        ######################## by spline ###########################
+
+        # bias: b_u
+        total_mean = np.mean(self._data['rating'])
+        user_mean = self._data.groupby('userId').mean()['rating']
+        user_bias = pd.DataFrame(user_mean - total_mean)
+        self._user_bias = pd.DataFrame(user_mean - total_mean)
+        # Prep for splines
+        user_rating_num = self._data.groupby(['userId','timestamp']).count()
+        N_vec = []
+        for user_id in self._data['userId'].unique():  
+          N_vec.append(user_rating_num[user_rating_num.index.get_level_values(0)==user_id]['movieId'].sum())
+
+        K_vec = np.round(np.power(N_vec,power))
+
+        data_sort = self._data[['userId','timestamp','rating']].sort_values(by=['userId','timestamp'])
+        new_index = []
+        for user_id in self._data['userId'].unique():
+          new_index.extend(list(range(1,N_vec[user_id-1]+1)))
+        data_sort['old_index'] = data_sort.index
+        data_sort.index = new_index
+
+        step_vec = np.floor(N_vec/K_vec)
+        user_time_bias = pd.Series([])
+
+        for user_id in data_sort['userId'].unique():
+         A = np.linspace(1,K_vec[user_id-1],int(K_vec[user_id-1]))*step_vec[user_id-1]
+         A = A.astype(int).tolist()
+         B = [1]
+         B.extend(A)
+
+         control_time = data_sort[data_sort['userId']==user_id]['timestamp'][B]
+         control_time_bias = data_sort[data_sort['userId']==user_id]['rating'][B] - total_mean
+
+         denom = 0
+         numer = 0
+         for i in range(len(B)):
+           time_dev_abs = np.abs(data_sort[data_sort['userId']==user_id]['timestamp'] - control_time.iloc[i])
+           denom = denom + np.exp(-gamma*time_dev_abs)
+           numer = numer + np.exp(-gamma*time_dev_abs)*control_time_bias.iloc[i]
+         spline_frac = numer/denom
+         time_bias = spline_frac.fillna(0) + user_bias.iloc[user_id-1][0]
+         user_time_bias = user_time_bias.append(time_bias)
+
+        data_sort['user_time_bias'] = user_time_bias.tolist()
+        data_sort.index = data_sort['old_index'].tolist()
+        self._user_time_bias = data_sort.sort_index()[['userId','timestamp','user_time_bias']]
+
+        sample_index = [index for index in self._train_data.index]
+
+        for m in range(maxiter):
+            sample_batch = random.sample(sample_index, batch)
+            # loop through each training case and perform update
+            for index in sample_batch:
+                u = self._train_data.loc[index]['userId']
+                u_time = self._train_data.loc[index]['timestamp']
+                i = self._train_data.loc[index]['movieId']
+                i_bin = (self._train_data.loc[index]['timestamp']-self._data['timestamp'].min())//bin_width
+                bias_u = self._user_time_bias[(self._user_time_bias['userId']==u)&(self._user_time_bias['timestamp']==u_time)]['user_time_bias'].iloc[0]
+                bias_i = self._item_bin_bias[i,i_bin]
+                r_ui = self._train_data.loc[index]['rating']
+                e_ui = r_ui - total_mean-bias_u -bias_i- np.dot(item_latent[[i]].T, user_latent[[u]])
+
+                grad_user_latent = np.array(e_ui * user_latent[[u]]) - np.array(lam * item_latent[[i]])
+                if (all(np.abs(grad) > stopping_deriv for grad in grad_user_latent)):
+                    item_latent[[i]] = item_latent[[i]] + lrate * grad_user_latent
+
+                grad_item_latent = np.array(e_ui * item_latent[[i]]) - np.array(lam * user_latent[[u]])
+                if (all(np.abs(grad) > stopping_deriv for grad in grad_item_latent)):
+                    user_latent[[u]] = user_latent[[u]] + lrate * grad_item_latent
+
+                grad_user_bias = e_ui - lam*bias_u
+                if (all(np.abs(grad) > stopping_deriv for grad in grad_user_bias)):
+                    self._user_bias.loc[u]['rating'] = bias_u + lrate * grad_user_bias
+
+                grad_item_bias = e_ui - lam * bias_i
+                if (all(np.abs(grad) > stopping_deriv for grad in grad_item_bias)):
+                    self._item_bias.loc[i]['rating'] = bias_u + lrate * grad_item_bias
+
+            self._user_latent = user_latent
+            self._item_latent = item_latent
+
+            print(m, 'user_latent', user_latent)
 
     def calcSimMatrix(self):
         # similarity bewteen movie i1 and i2
